1f4c97b1513d6751e0f1237a4d936011f4b9453f
stp <stp@2lvl.is74.ru>
2020-01-02 16:35:20 +0500

    add admin app

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web_app',
     'crispy_forms',
     'mainland',
     'users',
     'quiz.apps.QuizConfig',
     # 'django-random-queryset',
+    'guardian',
 ]
 
 MIDDLEWARE = [
